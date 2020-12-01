8523548d16b18d424ec97dcd23eeb6a9204ca0b3
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2020-12-01 16:17:36 +0100

    Update UpAndDown.hpp

--- src/engine/utils/UpAndDown.hpp
+++ src/engine/utils/UpAndDown.hpp
@@ -1,32 +1,17 @@
-#include "UpAndDown.hpp"
-#include <engine\graphics\core\texture.cpp>
-#include <engine\graphics\renderer\mesh.cpp>
-#include <engine\graphics\renderer\meshrenderer.hpp>
-#include <iostream>
+#include <engine/utils/GameState.hpp>
+#pragma once
+class UpAndDown : public GameState {
+public:
+	UpAndDown();
+	//has pure virtual functions for frame updates
+	void update(float _time, float _deltaTime);
+	void draw(float _time, float _deltaTime);
 
-UpAndDown::UpAndDown() : GameState() {
-	difference = glm::translate(glm::vec3(0.f, 0.f, -5.0f));
-}
+	//has virtual interface for state transition events
+	void onResume() ;
+	void onPause() ;
 
-void UpAndDown::newState() {
-	difference = glm::mat4(1.f);
-}
-
-void UpAndDown::update(float _time, float _deltaTime) {
-	float t = _time * _deltaTime;
-}
-
-void UpAndDown::draw(float _time, float _deltaTime) {
-	difference = difference * glm::translate(glm::vec3(0.f, 0.f, -1.1f));
-	renderer->clear();
-	renderer->draw(*mesh, *texture, difference);
-	renderer->present(*camera);
-}
-
-void UpAndDown::onResume() {
-	float t = 0;
-}
-
-void UpAndDown::onPause() {
-	float t = 0;
-}
+	//interface to mark current state as finished and to provide a new state
+	void newState() ;
+	bool isFinished();
+};
