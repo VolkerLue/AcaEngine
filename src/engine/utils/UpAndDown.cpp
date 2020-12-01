79d887eb4303ea6aaca6b7ab2c7ec24e7e692613
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2020-12-01 23:04:53 +0100

    Update UpAndDown.cpp

--- src/engine/utils/UpAndDown.cpp
+++ src/engine/utils/UpAndDown.cpp
@@ -1,32 +1,43 @@
-#include "UpAndDown.hpp"
-#include <engine\graphics\core\texture.cpp>
-#include <engine\graphics\renderer\mesh.cpp>
-#include <engine\graphics\renderer\meshrenderer.hpp>
+#include <engine/utils/UpAndDown.hpp>
+#include <engine/graphics/core/texture.cpp>
+#include <engine/graphics/renderer/mesh.cpp>
+#include <engine/graphics/renderer/meshrenderer.hpp>
 #include <iostream>
+#pragma once
 
 UpAndDown::UpAndDown() : GameState() {
-	difference = glm::translate(glm::vec3(0.f, 0.f, -5.0f));
+	difference = glm::translate(glm::vec3(0.f, 4.f, -5.0f));
 }
 
 void UpAndDown::newState() {
-	difference = glm::mat4(1.f);
+	difference = glm::translate(glm::vec3(0.f, 4.f, -5.0f));
 }
 
 void UpAndDown::update(float _time, float _deltaTime) {
-	float t = _time * _deltaTime;
+	if (_time > 5) {
+		finished = true;
+	}
+	if ((int)_time % 2 == 0) {
+		difference *= glm::translate(glm::vec3(0.f, -0.05f, 0.f));
+	}
+	else {
+		difference *= glm::translate(glm::vec3(0.f, +0.05f, 0.f));
+	}
 }
 
 void UpAndDown::draw(float _time, float _deltaTime) {
-	difference = difference * glm::translate(glm::vec3(0.f, 0.f, -1.1f));
 	renderer->clear();
 	renderer->draw(*mesh, *texture, difference);
 	renderer->present(*camera);
 }
 
 void UpAndDown::onResume() {
-	float t = 0;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	renderer->clear();
+	renderer->draw(*mesh, *texture, difference);
+	renderer->present(*camera);
 }
 
 void UpAndDown::onPause() {
-	float t = 0;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
