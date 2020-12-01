4de08daf2bd523a21aac5b554aeb3da93068be8b
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2020-12-01 23:05:02 +0100

    Update UpAndDown.hpp

--- src/engine/utils/UpAndDown.hpp
+++ src/engine/utils/UpAndDown.hpp
@@ -4,14 +4,14 @@ class UpAndDown : public GameState {
 public:
 	UpAndDown();
 	//has pure virtual functions for frame updates
-	void update(float _time, float _deltaTime);
-	void draw(float _time, float _deltaTime);
+	void update(float _time, float _deltaTime) override;
+	void draw(float _time, float _deltaTime)override;
 
 	//has virtual interface for state transition events
-	void onResume();
-	void onPause();
+	void onResume() override;
+	void onPause() override;
 
 	//interface to mark current state as finished and to provide a new state
-	void newState();
+	void newState() override;
 	bool isFinished();
 };
