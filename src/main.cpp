0beb927508876def6a9ca35267b5ce93a5d64ab2
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2020-12-01 23:01:23 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,9 @@
 #include <thread>
 #include <glm\ext\matrix_transform.hpp>
 #include <engine/utils/Game.hpp>
+//#include <engine/utils/Spring.cpp>
 #include <engine/utils/UpAndDown.hpp>
+#include <engine/utils/FreeFall.hpp>
 #include <memory>
 #include <chrono>
 #pragma once
@@ -39,8 +41,14 @@ int main(int argc, char* argv[])
 #endif
 #endif
 	Game game;
-	UpAndDown a;
-	game.run(std::make_unique<UpAndDown>(a));
+	//UpAndDown upanddown;
+	//game.run(std::make_unique<UpAndDown>());
+	//FreeFall freefall;
+	game.run(std::make_unique<FreeFall>());
+	//Spring spring;
+	//game.run(std::make_unique<Spring>());
+	//game.run(std::make_unique<UpAndDown>(upanddown));
+	
 
 	/*
 	glm::vec3 planetPositions[] = {
@@ -55,17 +63,13 @@ int main(int argc, char* argv[])
 		glm::vec3(1.5f,  7.0f, -1.5f),
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
-
 	graphics::Device::initialize(1366, 768, false);
 	GLFWwindow* window = graphics::Device::getWindow();
 	input::InputManager::initialize(window);
-
 	using namespace graphics;
 	Camera camera(45.f, 0.1f, 10000.f);
-
 	glm::mat4 view = camera.getView();
 	glm::mat4 model = glm::mat4(1.0f);
-
 	Sampler sampler(Sampler::Filter::LINEAR, Sampler::Filter::LINEAR, Sampler::Filter::LINEAR);
 	const Texture2D& texturePlanet = *Texture2DManager::get("textures/planet1.png", sampler);
 	const Texture2D& textureCrate = *Texture2DManager::get("textures/cratetex.png", sampler);
@@ -74,13 +78,10 @@ int main(int argc, char* argv[])
 	MeshRenderer renderer[10];
 	for (int i = 0; i < 10; i++)
 		renderer[i].draw(meshPlanet, texturePlanet, glm::mat4(1.f));
-
 	auto lastCursorPos = input::InputManager::getCursorPos();
-
 	float lastTime = glfwGetTime();
 	float currentTime = glfwGetTime();
 	float deltaTime = glfwGetTime() - lastTime;
-
 	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -92,56 +93,41 @@ int main(int argc, char* argv[])
 	glm::vec3 direction;
 	float yaw = -90.0f;
 	float pitch = 0.0f;
-
 	const float mouseSensitivity = 3.0f;
 	const float cameraSpeed = 3.0f;
-
 	float angle = 1.0f;
-
 	input::InputManager::setCursorMode(input::InputManager::CursorMode::DISABLED);
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		currentTime = glfwGetTime();
 		deltaTime = currentTime - lastTime;
 		lastTime = currentTime;
-
 		using namespace input;
-
 		if (InputManager::isKeyPressed(input::Key::ESCAPE))		glfwSetWindowShouldClose(window, true);
 		if (InputManager::isKeyPressed(input::Key::W))			cameraPos += cameraSpeed * cameraFront * deltaTime;
 		if (InputManager::isKeyPressed(input::Key::S))			cameraPos -= cameraSpeed * cameraFront * deltaTime;
 		if (InputManager::isKeyPressed(input::Key::A))			cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed * deltaTime;
 		if (InputManager::isKeyPressed(input::Key::D))			cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed * deltaTime;
-
 		if (InputManager::getCursorPos() != lastCursorPos) {
-
 			float deltaX = InputManager::getCursorPos().x - lastCursorPos.x;
 			float deltaY = lastCursorPos.y - InputManager::getCursorPos().y;
-
 			lastCursorPos.y = InputManager::getCursorPos().y;
 			lastCursorPos.x = InputManager::getCursorPos().x;
-
 			yaw += deltaX * mouseSensitivity * deltaTime;
 			pitch += deltaY * mouseSensitivity * deltaTime;
-
 			if (pitch > 89.0f)
 				pitch = 89.0f;
 			if (pitch < -89.0f)
 				pitch = -89.0f;
-
 			glm::vec3 direction;
 			direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
 			direction.y = sin(glm::radians(pitch));
 			direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-
 			cameraFront = glm::normalize(direction);
 		}
-
 		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		camera.setView(view);
-
 		for (unsigned int i = 0; i < 10; i++) {
 			glm::mat4 model = glm::mat4(1.0f);
 			if (angle >= 360.0) angle -= 360.0;
@@ -151,16 +137,13 @@ int main(int argc, char* argv[])
 			camera.setView(camera.getView() * model);
 			renderer[i].present(camera);
 		}
-
 		std::cout << deltaTime << "\n";
-
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_CULL_FACE);
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 		std::this_thread::sleep_for(8ms);
 	}
-
 	utils::MeshLoader::clear();
 	graphics::Device::close();*/
 	return EXIT_SUCCESS;
