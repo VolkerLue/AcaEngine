30e8390ac2985fc22bdb2130f2eb73683b5fca32
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2020-12-02 12:04:22 +0100

    Update Spring.hpp

--- src/engine/utils/Spring.hpp
+++ src/engine/utils/Spring.hpp
@@ -1,18 +1,19 @@
-#include <engine/utils/GameState.hpp>
 #pragma once
+#include <engine/utils/GameState.hpp>
+
 class Spring : public GameState {
 public:
 	Spring();
 	//has pure virtual functions for frame updates
-	void update(float _time, float _deltaTime);
-	void draw(float _time, float _deltaTime);
+	void update(float _time, float _deltaTime) override;
+	void draw(float _time, float _deltaTime) override;
 
 	//has virtual interface for state transition events
-	void onResume();
-	void onPause();
+	void onResume() override;
+	void onPause() override;
 
 	//interface to mark current state as finished and to provide a new state
-	void newState();
+	void newState() override;
 	bool isFinished();
 	float times;
 };
