9f647314c00de7f1fb733250fdf213f2e611e506
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2020-12-02 13:30:03 +0100

    Update meshrenderer.hpp

--- src/engine/graphics/renderer/meshrenderer.hpp
+++ src/engine/graphics/renderer/meshrenderer.hpp
@@ -5,6 +5,7 @@
 #include "glm/glm.hpp"
 #include "mesh.hpp"
 #include <vector>
+#include <engine\graphics\core\geometrybuffer.hpp>
 
 namespace graphics {
 
@@ -16,6 +17,8 @@ namespace graphics {
 	public:
 		MeshRenderer();
 
+		~MeshRenderer();
+
 		void draw(const Mesh& _mesh, const Texture2D& _texture, const glm::mat4& _transform);
 
 		void present(const Camera& _camera);
@@ -24,13 +27,12 @@ namespace graphics {
 	private:
 
 		struct MeshInstance {
-			std::vector<Mesh::Vertex> meshVertices;
+			const std::vector<Mesh::Vertex>& meshVertices;
 			const Texture2D& texture;
-			const glm::mat4& transform;
-			unsigned vao;
-			unsigned vbo;
-			mutable bool dirty;
+			glm::mat4 transform;
 		};
+
+		graphics::GeometryBuffer* geometryBuffer;
 
 		std::vector<MeshInstance> instances;
 
