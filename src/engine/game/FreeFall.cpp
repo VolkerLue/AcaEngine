8bae169eb405a4eaf47ca31efe87eb20c72242ab
VolkerLue <65592732+VolkerLue@users.noreply.github.com>
2021-01-03 16:56:31 +0100

    Update FreeFall.cpp

--- src/engine/game/FreeFall.cpp
+++ src/engine/game/FreeFall.cpp
@@ -1,44 +1,55 @@
-#pragma once
 #include "FreeFall.hpp"
 #include <GL/glew.h>
 
-FreeFall::FreeFall() : GameState(), a(-0.01f), v(0.f),
-	difference(glm::mat4(1.f)),
+struct Mesh {
+	graphics::Mesh mesh;
+};
+
+struct Transfrom {
+	glm::mat4 transfrom;
+};
+
+FreeFall::FreeFall() :	GameState(),
+	a(-0.01f), 
+	v(0.f),
 	camera(graphics::Camera(45.f, 0.1f, 10000.f)),
-	texture(*graphics::Texture2DManager::get("textures/planet1.png",
-	graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
-	mesh(graphics::Mesh(*utils::MeshLoader::get("models/sphere.obj"))),
+	texture(*graphics::Texture2DManager::get("textures/planet1.png", 
+		graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
 	renderer(graphics::MeshRenderer()) 
 {
 	utils::MeshLoader::clear();
-	difference = glm::translate(glm::vec3(0.f, 10.f, -20.0f));
+
+	planet1 = registry.create();
+	registry.addComponent<Mesh>(planet1, *utils::MeshLoader::get("models/sphere.obj"));
+	registry.addComponent<Transfrom>(planet1, glm::translate(glm::vec3(0.f, 5.f, -10.0f)));
 }
 
 void FreeFall::newState() {
-	difference = glm::translate(glm::vec3(0.f, 10.f, -20.0f));
+	registry.addComponent<Transfrom>(planet1, glm::translate(glm::vec3(0.f, 5.f, -10.0f)));
 }
 
 void FreeFall::update(float _time, float _deltaTime) {
 	if (_time > 15) {
 		finished = true;
 	}
+	v = v + a * _deltaTime;
+	registry.execute<Transfrom>([&](Transfrom& trans) {trans.transfrom = trans.transfrom * glm::translate(glm::vec3(0.f, v, 0.f)); });
+
+	if (registry.getComponent<Transfrom>(planet1)->transfrom[3][1] <= -5.f) {
+		registry.execute<Transfrom>([&](Transfrom& trans) {trans.transfrom = glm::translate(glm::vec3(0.f, 5.f, -10.0f)); });
+	}
 }
 
 void FreeFall::draw(float _time, float _deltaTime) {
-	v = v + a * _deltaTime;
-	difference = difference * glm::translate(glm::vec3(0.f, v, 0.f));
-	if (difference[3][1] <= -10.f) {
-		difference = glm::translate(glm::vec3(0.f, 10.f, -20.0f));
-	}
 	renderer.clear();
-	renderer.draw(mesh, texture, difference);
+	renderer.draw(registry.getComponent<Mesh>(planet1)->mesh, texture, registry.getComponent<Transfrom>(planet1)->transfrom);
 	renderer.present(camera);
 }
 
 void FreeFall::onResume() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	renderer.clear();
-	renderer.draw(mesh, texture, difference);
+	renderer.draw(registry.getComponent<Mesh>(planet1)->mesh, texture, registry.getComponent<Transfrom>(planet1)->transfrom);
 	renderer.present(camera);
 }
 
