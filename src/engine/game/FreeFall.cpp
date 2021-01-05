d4c211035f40f27541509e7f3da00fa8be249a1b
Jusseh <73781543+Jusseh@users.noreply.github.com>
2021-01-05 17:30:57 +0100

    Update FreeFall.cpp

--- src/engine/game/FreeFall.cpp
+++ src/engine/game/FreeFall.cpp
@@ -9,13 +9,20 @@ struct Transfrom {
 	glm::mat4 transfrom;
 };
 
-FreeFall::FreeFall() :	GameState(),
-	a(-0.01f), 
-	v(0.f),
-	camera(graphics::Camera(45.f, 0.1f, 10000.f)),
-	texture(*graphics::Texture2DManager::get("textures/planet1.png", 
-		graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
-	renderer(graphics::MeshRenderer()) 
+struct Velocity {
+	glm::vec3 velo;
+};
+
+struct Accelaration {
+	glm::vec3 accel;
+};
+
+FreeFall::FreeFall() : GameState(),
+
+camera(graphics::Camera(45.f, 0.1f, 10000.f)),
+texture(*graphics::Texture2DManager::get("textures/planet1.png",
+	graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
+	renderer(graphics::MeshRenderer())
 {
 	utils::MeshLoader::clear();
 
@@ -26,14 +33,16 @@ FreeFall::FreeFall() :	GameState(),
 
 void FreeFall::newState() {
 	registry.addComponent<Transfrom>(planet1, glm::translate(glm::vec3(0.f, 5.f, -10.0f)));
+	registry.addComponent<Velocity>(planet1, glm::vec3(0.f, 0.f, 0.f));
+	registry.addComponent<Accelaration>(planet1, glm::vec3(0.f, -0.01f, 0.f));
 }
 
 void FreeFall::update(float _time, float _deltaTime) {
 	if (_time > 15) {
 		finished = true;
 	}
-	v = v + a * _deltaTime;
-	registry.execute<Transfrom>([&](Transfrom& trans) {trans.transfrom = trans.transfrom * glm::translate(glm::vec3(0.f, v, 0.f)); });
+	registry.execute<Velocity, Accelaration>([&](Velocity& velo, Accelaration& accel) {velo.velo = velo.velo + accel.accel * _deltaTime; });
+	registry.execute<Transfrom, Velocity>([&](Transfrom& trans, Velocity& velo) {trans.transfrom = trans.transfrom * glm::translate(velo.velo); });
 
 	if (registry.getComponent<Transfrom>(planet1)->transfrom[3][1] <= -5.f) {
 		registry.execute<Transfrom>([&](Transfrom& trans) {trans.transfrom = glm::translate(glm::vec3(0.f, 5.f, -10.0f)); });
@@ -56,3 +65,4 @@ void FreeFall::onResume() {
 void FreeFall::onPause() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
+
