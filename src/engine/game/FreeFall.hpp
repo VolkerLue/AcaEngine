bb132287da78e346d66ec5834916ab94fb05c10c
VolkerLue <65592732+VolkerLue@users.noreply.github.com>
2021-01-10 16:43:48 +0100

    Update FreeFall.hpp

--- src/engine/game/FreeFall.hpp
+++ src/engine/game/FreeFall.hpp
@@ -1,29 +1,23 @@
 #pragma once
 #include "GameState.hpp"
-#include "Registry2.hpp"
+#include "../systems/System.hpp"
+
 
 class FreeFall : public GameState {
 public:
 	FreeFall();
-	//has pure virtual functions for frame updates
+
 	void update(float _time, float _deltaTime) override;
 	void draw(float _time, float _deltaTime) override;
 
-	//has virtual interface for state transition events
 	void onResume() override;
 	void onPause() override;
 
-	//interface to mark current state as finished and to provide a new state
 	void newState() override;
 	bool isFinished();
 
 private:
+	Entity entity;
+	System system;
 	const graphics::Texture2D& texture;
-	graphics::Camera camera;
-	graphics::MeshRenderer renderer;
-	float a;
-	float v;
-
-	Registry2 registry;
-	Entity planet1;
 };
