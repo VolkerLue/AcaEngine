f7942851c96c2aaaa1a2e7d1fe6976360f675c4c
VolkerLue <65592732+VolkerLue@users.noreply.github.com>
2021-01-10 16:43:08 +0100

    Update FreeFall.cpp

--- src/engine/game/FreeFall.cpp
+++ src/engine/game/FreeFall.cpp
@@ -1,68 +1,38 @@
 #include "FreeFall.hpp"
 #include <GL/glew.h>
 
-struct Mesh {
-	graphics::Mesh mesh;
-};
-
-struct Transfrom {
-	glm::mat4 transfrom;
-};
-
-struct Velocity {
-	glm::vec3 velo;
-};
-
-struct Accelaration {
-	glm::vec3 accel;
-};
-
-FreeFall::FreeFall() : GameState(),
-
-camera(graphics::Camera(45.f, 0.1f, 10000.f)),
-texture(*graphics::Texture2DManager::get("textures/planet1.png",
-	graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
-	renderer(graphics::MeshRenderer())
+FreeFall::FreeFall() : GameState(), system(), texture(*graphics::Texture2DManager::get("textures/planet1.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR)))
 {
-	utils::MeshLoader::clear();
-
-	planet1 = registry.create();
-	registry.addComponent<Mesh>(planet1, *utils::MeshLoader::get("models/sphere.obj"));
-	registry.addComponent<Transfrom>(planet1, glm::translate(glm::vec3(0.f, 5.f, -10.0f)));
+	entity = system.createEntity(entity);
+	system.addMesh(entity, "models/sphere.obj");
+	system.addTransform(entity, glm::translate(glm::vec3(0.f, 10.f, -50.f)));
+	system.addVelocity(entity, glm::vec3(0.f, 0.13f, 0.f));
+	system.addAccelaration(entity, glm::vec3(0.f, -0.1f, 0.f));
 }
 
 void FreeFall::newState() {
-	registry.addComponent<Transfrom>(planet1, glm::translate(glm::vec3(0.f, 5.f, -10.0f)));
-	registry.addComponent<Velocity>(planet1, glm::vec3(0.f, 0.f, 0.f));
-	registry.addComponent<Accelaration>(planet1, glm::vec3(0.f, -0.01f, 0.f));
 }
 
 void FreeFall::update(float _time, float _deltaTime) {
-	if (_time > 15) {
+	if (_time > 5) {
 		finished = true;
 	}
-	registry.execute<Velocity, Accelaration>([&](Velocity& velo, Accelaration& accel) {velo.velo = velo.velo + accel.accel * _deltaTime; });
-	registry.execute<Transfrom, Velocity>([&](Transfrom& trans, Velocity& velo) {trans.transfrom = trans.transfrom * glm::translate(velo.velo); });
-
-	if (registry.getComponent<Transfrom>(planet1)->transfrom[3][1] <= -5.f) {
-		registry.execute<Transfrom>([&](Transfrom& trans) {trans.transfrom = glm::translate(glm::vec3(0.f, 5.f, -10.0f)); });
-	}
+	system.move(entity, _deltaTime);
 }
 
 void FreeFall::draw(float _time, float _deltaTime) {
-	renderer.clear();
-	renderer.draw(registry.getComponent<Mesh>(planet1)->mesh, texture, registry.getComponent<Transfrom>(planet1)->transfrom);
-	renderer.present(camera);
+	system.draw(entity, texture);
 }
 
 void FreeFall::onResume() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	renderer.clear();
-	renderer.draw(registry.getComponent<Mesh>(planet1)->mesh, texture, registry.getComponent<Transfrom>(planet1)->transfrom);
-	renderer.present(camera);
+	system.draw(entity, texture);
 }
 
 void FreeFall::onPause() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
+bool FreeFall::isFinished(){
+	return false;
+}
