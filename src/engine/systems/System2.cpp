976bb0dd98a92a034b34f1ffa5a7519e1222f9a2
VolkerLue <65592732+VolkerLue@users.noreply.github.com>
2021-01-19 18:07:58 +0100

    Update System2.cpp

--- src/engine/systems/System2.cpp
+++ src/engine/systems/System2.cpp
@@ -1,7 +1,7 @@
 #include "System2.hpp"
 
 System2::System2() : registry(),
-camera(graphics::Camera(45.f, 0.1f, 10000.f)),
+camera(graphics::Camera(45.f, 0.1f, 100.f)),
 renderer(graphics::MeshRenderer())
 {
 	utils::MeshLoader::clear();
@@ -24,7 +24,107 @@ std::optional<Entity> System2::getEntity(EntityRef _entity) const {
 	return registry.getEntity(_entity);
 }
 
+
 /* ################ Draw-System ################ */
+void System2::drawEntity(Entity& _entity, const graphics::Texture2D& _texture) {
+	renderer.clear();
+	renderer.draw(registry.getComponent<Mesh>(_entity)->mesh, _texture, registry.getComponent<Transform>(_entity)->transform);
+	renderer.present(camera);
+}
+
+void System2::setCamera(float _fov, float _zNear, float zFar) {
+	camera = graphics::Camera(_fov, _zNear, zFar);
+}
+
+void System2::repositionCrate() {
+	registry.execute<Transform, Rotation>([&](Transform& transform, const Rotation& rotation) {
+		glm::vec3 position = glm::normalize(glm::vec3(transform.transform[3][0], transform.transform[3][1], transform.transform[3][2]));
+		if (position[0] < (-1) || position[0] > (1) ||
+			position[1] < (-1) || position[1] > (1) ||
+			position[2] < (-1) || position[2] > (0))
+		{
+			transform.transform = glm::translate(glm::vec3(0.f, 0.f, float(rand() % 30 + (-90))));
+		}});
+};
+
+/* ################ Physic-System ################ */
+void System2::move(Entity& _entity, float _deltaTime) {
+	glm::vec3 velocity = registry.getComponent<Velocity>(_entity)->velocity +
+		registry.getComponent<Accelaration>(_entity)->accelaration * _deltaTime;
+	setVelocity(_entity, velocity);
+	transfromMultiply(_entity, glm::translate(velocity));
+}
+
+void System2::springY(Entity& _entity, float _deltaTime) {
+	float k = 20.f;
+
+	glm::mat4 transfrom = registry.getComponent<Transform>(_entity)->transform;
+	float positionEntityY = transfrom[3][1];
+	float positionAnchorY = registry.getComponent<Anchor>(_entity)->anchor[1];
+	float springForceY = -k * (positionEntityY - positionAnchorY);
+
+	float mass = registry.getComponent<Mass>(_entity)->mass;
+	float gravity = registry.getComponent<Accelaration>(_entity)->accelaration[0];
+	float forceY = springForceY + mass * gravity;
+
+	float accelerationY = forceY / mass;
+	float velocityY = registry.getComponent<Velocity>(_entity)->velocity[1];
+	velocityY += accelerationY * _deltaTime;
+	setVelocity(_entity, glm::vec3(0.f, velocityY, 0.f));
+	positionEntityY += velocityY * _deltaTime;
+
+	transfrom[3][1] = positionEntityY;
+	setTransform(_entity, transfrom);
+}
+
+void System2::transfromMultiply(Entity& _entity, glm::mat4 _transform) {
+	Transform& transform = registry.getComponentUnsafe<Transform>(_entity);
+	transform.transform *= _transform;
+}
+
+void System2::updateTransformCrate(float _deltaTime) {
+	registry.execute<Transform, Velocity, Rotation>([&](
+		Transform& transform, const Velocity& velocity, const Rotation& rotation) {
+			transform.transform *= glm::translate(velocity.velocity * _deltaTime),
+				transform.transform = glm::rotate(transform.transform, rotation.angleInRadians * _deltaTime, rotation.axisOfRotation);
+		});
+}
+
+void System2::updateTransformPlanet(float _deltaTime) {
+	registry.execute<Transform, Velocity>([&](
+		Transform& transform, const Velocity& velocity) {
+			transform.transform *= glm::translate(velocity.velocity * _deltaTime);
+		});
+}
+
+void System2::updateShoot(std::vector<Entity> entities) {
+
+	registry.execute<Transform, Alive, CursorPosition>([&](Transform& transform, Alive& alive, const CursorPosition& cursorPosition) {
+		glm::vec3 position = glm::normalize(glm::vec3(transform.transform[3][0], transform.transform[3][1], transform.transform[3][2]));
+		if (position[0] < (-1) || position[0] > (1) ||
+			position[1] < (-1) || position[1] > (1) ||
+			position[2] < (-1) || position[2] > (0))
+		{
+			alive.alive = false;
+		}});
+
+	int i;
+	if (inputManager.isButtonPressed(input::MouseButton::LEFT)) {
+		glm::vec3 curserPosition = camera.toWorldSpace(inputManager.getCursorPos());		
+		for (i = 100; i < 200; i++) {
+			if (registry.getComponentUnsafe<Alive>(entities[i]).alive == false){ break; }	
+			if (i == 199) { i++; }
+		}
+		if (i < 200) {
+			setAlive(entities[i], true);
+			setTransform(entities[i], glm::translate(curserPosition));
+			setVelocity(entities[i], glm::vec3(curserPosition[0] * 500, curserPosition[1] * 500, curserPosition[2] * 500));
+		}
+	}
+}
+
+
+/* ################ Component-System ################ */
 void System2::addMesh(Entity& _entity, const char* _mesh) {
 	registry.addComponent<Mesh>(_entity, *utils::MeshLoader::get(_mesh));
 }
@@ -38,22 +138,6 @@ void System2::setTransform(Entity& _entity, glm::mat4 _transform) {
 	transform.transform = _transform;
 }
 
-void System2::transfromMultiply(Entity& _entity, glm::mat4 _transform) {
-	Transform& transform = registry.getComponentUnsafe<Transform>(_entity);
-	transform.transform *= _transform;
-}
-
-void System2::drawEntity(Entity& _entity, const graphics::Texture2D& _texture) {
-	renderer.clear();
-	renderer.draw(registry.getComponent<Mesh>(_entity)->mesh, _texture, registry.getComponent<Transform>(_entity)->transform);
-	renderer.present(camera);
-}
-
-void System2::setCamera(float _fov, float _zNear, float zFar) {
-	camera = graphics::Camera(_fov, _zNear, zFar);
-}
-
-/* ################ Physic-System ################ */
 void System2::addVelocity(Entity& _entity, glm::vec3 _velocity) {
 	registry.addComponent<Velocity>(_entity, _velocity);
 }
@@ -90,31 +174,40 @@ void System2::setAnchor(Entity& _entity, glm::vec3 _anchor) {
 	anchor.anchor = _anchor;
 }
 
-void System2::move(Entity& _entity, float _deltaTime) {
-	glm::vec3 velocity = registry.getComponent<Velocity>(_entity)->velocity +
-		registry.getComponent<Accelaration>(_entity)->accelaration * _deltaTime;
-	setVelocity(_entity, velocity);
-	transfromMultiply(_entity, glm::translate(velocity));
+void System2::addRotation(Entity& _entity, float _angleInRadians, glm::vec3 _axisOfRotation) {
+	registry.addComponent<Rotation>(_entity, _angleInRadians, _axisOfRotation);
 }
 
-void System2::springY(Entity& _entity, float _deltaTime) {
-	float k = 20.f;
+void System2::setRotation(Entity& _entity, float _angleInRadians, glm::vec3 _axisOfRotation) {
+	Rotation& rotation = registry.getComponentUnsafe<Rotation>(_entity);
+	rotation.angleInRadians = _angleInRadians;
+	rotation.axisOfRotation = _axisOfRotation;
+}
 
-	glm::mat4 transfrom = registry.getComponent<Transform>(_entity)->transform;
-	float positionEntityY = transfrom[3][1];
-	float positionAnchorY = registry.getComponent<Anchor>(_entity)->anchor[1];
-	float springForceY = -k * (positionEntityY - positionAnchorY);
+void System2::addCursorPosition(Entity& _entity, glm::vec3 _curserPosition) {
+	registry.addComponent<CursorPosition>(_entity, _curserPosition);
+}
 
-	float mass = registry.getComponent<Mass>(_entity)->mass;
-	float gravity = registry.getComponent<Accelaration>(_entity)->accelaration[0];
-	float forceY = springForceY + mass * gravity;
+void System2::setCursorPosition(Entity& _entity, glm::vec3 _curserPosition) {
+	CursorPosition& curserPositon = registry.getComponentUnsafe<CursorPosition>(_entity);
+	curserPositon.curserPosition = _curserPosition;
+}
 
-	float accelerationY = forceY / mass;
-	float velocityY = registry.getComponent<Velocity>(_entity)->velocity[1];
-	velocityY += accelerationY * _deltaTime;
-	setVelocity(_entity, glm::vec3(0.f, velocityY, 0.f));
-	positionEntityY += velocityY * _deltaTime;
+void System2::addAlive(Entity& _entity, bool _alive) {
+	registry.addComponent<Alive>(_entity, _alive);
+}
 
-	transfrom[3][1] = positionEntityY;
-	setTransform(_entity, transfrom);
+void System2::setAlive(Entity& _entity, bool _alive) {
+	Alive& alive = registry.getComponentUnsafe<Alive>(_entity);
+	alive.alive = _alive;
+}
+
+
+/* ################ Utils-System ################ */
+int System2::randomWithoutZero(int quantity, int start) {
+	int random = 0;
+	while (random == 0) {
+		random = rand() % quantity + (start);
+	};
+	return random;
 }
