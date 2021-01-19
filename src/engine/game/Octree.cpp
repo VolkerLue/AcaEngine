50c20e439b9ee5583617305cdc7ffaa11e6cdc19
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2021-01-19 23:31:02 +0100

    Update Octree.cpp

--- src/engine/game/Octree.cpp
+++ src/engine/game/Octree.cpp
@@ -6,7 +6,9 @@ OctreeNode::OctreeNode(AABB box) {
 	isRoot = true;
 	hasChildren = false;
 	hasContent = false;
-	ent = Entity{ -1 };
+	content = nullptr;
+	uint32_t e = -1;
+	ent = Entity{ e };
 }
 
 OctreeNode::OctreeNode(OctreeNode* parent, AABB box) {
@@ -15,7 +17,8 @@ OctreeNode::OctreeNode(OctreeNode* parent, AABB box) {
 	isRoot = false;
 	hasChildren = false;
 	hasContent = false;
-	ent = Entity{ -1 };
+	uint32_t e = -1;
+	ent = Entity{ e };
 }
 
 OctreeNode::~OctreeNode()
@@ -28,14 +31,14 @@ OctreeNode::~OctreeNode()
 }
 
 
-void OctreeNode::insert(const AABB& other, Entity otherEnt, Registry2& registry) {
-	if (!contains(this->box, other)) return;
+void OctreeNode::insert(AABB& other, Entity otherEnt, Registry2& registry) {
+	if (!this->box.contains(this->box, other)) return;
 	if (hasContent) {
-		if (this->content->type == 2 && other->type == 1) {
-			Alive alive = registry.getComponent<Alive>(this->ent);
+		if (this->content->type == 2 && other.type == 1) {
+			Alive& alive = registry.getComponentUnsafe<Alive>(this->ent);
 			alive.alive = false;
-		} else if (this->content->type == 1 && other->type == 2) {
-			Alive alive = registry.getComponent<Alive>(otherEnt);
+		} else if (this->content->type == 1 && other.type == 2) {
+			Alive& alive = registry.getComponentUnsafe<Alive>(otherEnt);
 			alive.alive = false;
 		}
 		return;
@@ -46,19 +49,19 @@ void OctreeNode::insert(const AABB& other, Entity otherEnt, Registry2& registry)
 	int c = 0;
 	bool insertedChildren = false;
 	if (!hasChildren) {
-		children.push_back(new OctreeNode(this, AABB(3, center.x, this->box.maxX, center.y, this->box.maxY, center.z, this->box.maxZ)));
-		children.push_back(new OctreeNode(this, AABB(3, center.x, this->box.maxX, center.y, this->box.maxY, this->box.minZ, center.z)));
-		children.push_back(new OctreeNode(this, AABB(3, center.x, this->box.maxX, this->box.minY, center.y, center.z, this->box.maxZ)));
-		children.push_back(new OctreeNode(this, AABB(3, center.x, this->box.maxX, this->box.minY, center.y, this->box.minZ, center.z)));
-		children.push_back(new OctreeNode(this, AABB(3, this->box.minX, center.x, center.y, this->box.maxY, center.z, this->box.maxZ)));
-		children.push_back(new OctreeNode(this, AABB(3, this->box.minX, center.x, center.y, this->box.maxY, this->box.minZ, center.z)));
-		children.push_back(new OctreeNode(this, AABB(3, this->box.minX, center.x, this->box.minY, center.y, center.z, this->box.maxZ)));
-		children.push_back(new OctreeNode(this, AABB(3, this->box.minX, center.x, this->box.minY, center.y, this->box.minZ, center.z)));
+		children.push_back(new OctreeNode(this, AABB{ 3, center.x, this->box.maxX, center.y, this->box.maxY, center.z, this->box.maxZ }));
+		children.push_back(new OctreeNode(this, AABB{3, center.x, this->box.maxX, center.y, this->box.maxY, this->box.minZ, center.z }));
+		children.push_back(new OctreeNode(this, AABB{3, center.x, this->box.maxX, this->box.minY, center.y, center.z, this->box.maxZ }));
+		children.push_back(new OctreeNode(this, AABB{3, center.x, this->box.maxX, this->box.minY, center.y, this->box.minZ, center.z }));
+		children.push_back(new OctreeNode(this, AABB{3, this->box.minX, center.x, center.y, this->box.maxY, center.z, this->box.maxZ }));
+		children.push_back(new OctreeNode(this, AABB{3, this->box.minX, center.x, center.y, this->box.maxY, this->box.minZ, center.z }));
+		children.push_back(new OctreeNode(this, AABB{3, this->box.minX, center.x, this->box.minY, center.y, center.z, this->box.maxZ }));
+		children.push_back(new OctreeNode(this, AABB{3, this->box.minX, center.x, this->box.minY, center.y, this->box.minZ, center.z }));
 		hasChildren = true;
 		insertedChildren = true;
 	}
 	for (int i = 0; i < 8; i++) {
-		if (contains(children[i], other)) return children[i].insert(other);
+		if (this->content->contains(children[i]->box, other)) return children[i]->insert(other, otherEnt, registry);
 	}
 	if (insertedChildren) {
 		for (int i = 0; i < 8; i++) {
@@ -67,7 +70,7 @@ void OctreeNode::insert(const AABB& other, Entity otherEnt, Registry2& registry)
 		children.clear();
 		hasChildren = false;
 	}
-	content = other;
+	content = &other;
 	this->ent = otherEnt;
 	hasContent = true;
 }
