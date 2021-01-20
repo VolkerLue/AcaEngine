053c278ad4eef7ea89ea9630f998e5782afe32bc
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2021-01-20 13:39:35 +0100

    Update Octree.hpp

--- src/engine/game/Octree.hpp
+++ src/engine/game/Octree.hpp
@@ -43,6 +43,35 @@ struct AABB {
 			container.minY <= content.minY && container.maxY >= content.maxY &&
 			container.minZ <= content.minZ && container.maxZ >= content.maxZ);
 	}
+
+	AABB recalculateAABB(AABB aabb, const glm::mat4 transform) {
+		std::vector<glm::vec3> corners;
+		corners.push_back(glm::vec3(aabb.minX, aabb.minY, aabb.minZ));
+		corners.push_back(glm::vec3(aabb.minX, aabb.minY, aabb.maxZ));
+		corners.push_back(glm::vec3(aabb.minX, aabb.maxY, aabb.minZ));
+		corners.push_back(glm::vec3(aabb.minX, aabb.maxY, aabb.maxZ));
+		corners.push_back(glm::vec3(aabb.maxX, aabb.minY, aabb.minZ));
+		corners.push_back(glm::vec3(aabb.maxX, aabb.minY, aabb.maxZ));
+		corners.push_back(glm::vec3(aabb.maxX, aabb.maxY, aabb.minZ));
+		corners.push_back(glm::vec3(aabb.maxX, aabb.maxY, aabb.maxZ));
+		glm::vec3 current;
+		double minX = std::numeric_limits<double>::min();
+		double maxX = std::numeric_limits<double>::max();
+		double minY = std::numeric_limits<double>::min();
+		double maxY = std::numeric_limits<double>::max();
+		double minZ = std::numeric_limits<double>::min();
+		double maxZ = std::numeric_limits<double>::max();
+		for (auto it = corners.begin(); it != corners.end(); it++) {
+			current = glm::vec3(transform * glm::vec4(*it, 1));
+			if (current.x < minX) minX = current.x;
+			if (current.x > maxX) maxX = current.x;
+			if (current.y < minY) minY = current.y;
+			if (current.y > maxY) maxY = current.x;
+			if (current.z < minZ) minZ = current.z;
+			if (current.z > maxZ) maxZ = current.z;
+		}
+		return AABB{ aabb.type, minX, maxX, minY, maxY, minZ, maxZ };
+	}
 };
 
 
