954ec3c8350ffca545e186af895ec1e482e89f4f
VolkerLue <65592732+VolkerLue@users.noreply.github.com>
2021-01-23 16:42:21 +0100

    updateTransform() und draw() neu

--- src/engine/game/Shooter.cpp
+++ src/engine/game/Shooter.cpp
@@ -6,29 +6,16 @@ texturePlanet(*graphics::Texture2DManager::get("textures/planet1.png", graphics:
 textureCratetex(*graphics::Texture2DManager::get("textures/cratetex.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR)))
 {
 	srand(time(NULL));
-	for (int i = 0; i < 100; i++)
-	{
+	for (int i = 0; i < 100; i++){
 		Entity entity;
-		entities.push_back(system.createEntity(entity));
-				
+		entities.push_back(system.createEntity(entity));				
+
+		system.addTexture(entities.back(), &textureCratetex);
 		system.addMesh(entities.back(), "models/crate.obj");
 		system.addTransform(entities.back(), glm::translate(glm::vec3(0.f, 0.f, float(rand() % 10 + (-55)))));
-		system.addAABB(entities.back(), 2);
 		system.addVelocity(entities.back(), glm::vec3(system.randomWithoutZero(9, -4), system.randomWithoutZero(9, -4), system.randomWithoutZero(9, -4)));
-		system.addAlive(entities.back(), true);
 		system.addRotation(entities.back(), glm::radians(float(rand() % 20 + (10))), glm::vec3(rand() % 2, rand() % 2, rand() % 2));
-	}
-	for (int i = 0; i < 100; i++)
-	{
-		Entity entity;
-		entities.push_back(system.createEntity(entity));
-
-		system.addMesh(entities.back(), "models/sphere.obj");
-		system.addTransform(entities.back(), glm::translate(glm::vec3(0.f, 0.f, 0.f)));
-		system.addAABB(entities.back(), 1);
-		system.addVelocity(entities.back(), glm::vec3(0.f, 0.f, 0.f));
-		system.addAlive(entities.back(), false);
-		system.addCursorPosition(entities.back(), glm::vec3(0.f, 0.f, 0.f));
+		//system.addAABB(entities.back(), 2);
 	}
 }
 
@@ -36,24 +23,22 @@ void Shooter::newState() {
 }
 
 void Shooter::update(float _time, float _deltaTime) {
-	if (_time > 100) {
-		finished = true;
-	}	
-	system.updateTransformCrate(_deltaTime);	
-	system.updateTransformPlanet(_deltaTime);
-	system.updateAABB();
-	system.removeIntersecting();
+	if (_time > 100) finished = true;
+
+	system.updateTransform(_deltaTime);
+	system.removeEntityWhenNotInView(entities);
+	system.shootSphere(entities, 500.f, texturePlanet);
+	//system.updateAABB();
+	//system.removeIntersecting();
 }
 
 void Shooter::draw(float _time, float _deltaTime) {
-	system.draw<Mesh, Transform, Alive, Rotation>(textureCratetex);
-	system.draw<Mesh, Transform, Alive, CursorPosition>(texturePlanet);
+	system.draw();
 }
 
 void Shooter::onResume() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	system.draw<Mesh, Transform, Alive, Rotation>(textureCratetex);
-	system.draw<Mesh, Transform, Alive, CursorPosition>(texturePlanet);
+	system.draw();
 }
 
 void Shooter::onPause() {
