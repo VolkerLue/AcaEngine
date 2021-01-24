9cebfed6c4a3ee93f5092a56c1cf36deb8f9ecd3
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2021-01-24 22:03:29 +0100

    Update System2.hpp

--- src/engine/systems/System2.hpp
+++ src/engine/systems/System2.hpp
@@ -1,314 +1,110 @@
-#include "System2.hpp"
+#pragma once
+#include "Components.hpp"
+#include "../game/Registry2.hpp"
+#include "../graphics/core/texture.hpp"
+#include <engine/input/inputmanager.hpp>
+#include <glm/gtx/transform.hpp>
+#include <GL/glew.h>
+#include <limits>
+#include "../math/geometrictypes.hpp"
+#include "../utils/containers/octree.hpp"
+#include <set>
 
 
+class System2
+{	
+public:	
+	System2();
+
+	/* ################ Entity-System ################ */
+	Entity& createEntity(Entity& _entity);
+
+	void eraseEntity(Entity& _entity);
+
+	EntityRef getEntityReference(Entity _ent) const;
+
+	std::optional<Entity> getEntity(EntityRef _ent) const;
 
 
-System2::System2() : registry(),
-camera(graphics::Camera(45.f, 0.1f, 100.f)),
-renderer(graphics::MeshRenderer())
-{
-	utils::MeshLoader::clear();
-}
+	/* ################ Draw-System ################ */	
+	void draw();
 
-/* ################ Entity-System ################ */
-Entity& System2::createEntity(Entity& entity) {
-	return entity = registry.create();
-}
+	void drawEntity(Entity& _entity, const graphics::Texture2D& _texture);
 
-void System2::eraseEntity(Entity& _entity) {
-	registry.erase(_entity);
-}
-
-EntityRef System2::getEntityReference(Entity _entity) const {
-	return registry.getRef(_entity);
-}
-
-std::optional<Entity> System2::getEntity(EntityRef _entity) const {
-	return registry.getEntity(_entity);
-}
+	void setCamera(float _fov, float _zNear, float zFar);
 
 
-/* ################ Draw-System ################ */
-void System2::draw() {
-	renderer.clear();
-	registry.execute<Mesh, Texture, Transform>([&](
-		const Mesh& mesh, const Texture texture, const Transform& transform) {
-			renderer.draw(mesh.mesh, *texture.texture, transform.transform);
-		});
-	renderer.present(camera);
-}
+	/* ################ Physic-System ################ */	
+	void move(Entity& _entity, float _deltaTime);
 
-void System2::drawEntity(Entity& _entity, const graphics::Texture2D& _texture) {
-	renderer.clear();
-	renderer.draw(registry.getComponent<Mesh>(_entity)->mesh, _texture, registry.getComponent<Transform>(_entity)->transform);
-	renderer.present(camera);
-}
+	void springY(Entity& _entity, float _deltaTime);
 
-void System2::setCamera(float _fov, float _zNear, float zFar) {
-	camera = graphics::Camera(_fov, _zNear, zFar);
-}
+	void transfromMultiply(Entity& _entity, glm::mat4 _transform);
 
+	void updateTransform(float _deltaTime);
 
-/* ################ Physic-System ################ */
-void System2::move(Entity& _entity, float _deltaTime) {
-	glm::vec3 velocity = registry.getComponent<Velocity>(_entity)->velocity +
-		registry.getComponent<Accelaration>(_entity)->accelaration * _deltaTime;
-	setVelocity(_entity, velocity);
-	transfromMultiply(_entity, glm::translate(velocity));
-}
+	void removeEntityWhenNotInView(std::list<Entity>& entities);
 
-void System2::springY(Entity& _entity, float _deltaTime) {
-	float k = 20.f;
+	int whichEntityIsNotInView();
 
-	glm::mat4 transfrom = registry.getComponent<Transform>(_entity)->transform;
-	float positionEntityY = transfrom[3][1];
-	float positionAnchorY = registry.getComponent<Anchor>(_entity)->anchor[1];
-	float springForceY = -k * (positionEntityY - positionAnchorY);
-
-	float mass = registry.getComponent<Mass>(_entity)->mass;
-	float gravity = registry.getComponent<Accelaration>(_entity)->accelaration[0];
-	float forceY = springForceY + mass * gravity;
-
-	float accelerationY = forceY / mass;
-	float velocityY = registry.getComponent<Velocity>(_entity)->velocity[1];
-	velocityY += accelerationY * _deltaTime;
-	setVelocity(_entity, glm::vec3(0.f, velocityY, 0.f));
-	positionEntityY += velocityY * _deltaTime;
-
-	transfrom[3][1] = positionEntityY;
-	setTransform(_entity, transfrom);
-}
-
-void System2::transfromMultiply(Entity& _entity, glm::mat4 _transform) {
-	Transform& transform = registry.getComponentUnsafe<Transform>(_entity);
-	transform.transform *= _transform;
-}
-
-void System2::updateTransform(float _deltaTime) {
-	executeVelocity(_deltaTime);
-	executeRotation(_deltaTime);
-}
-
-void System2::executeVelocity(float _deltaTime) {
-	registry.execute<Transform, Velocity>([&](Transform& transform, Velocity& velocity) {
-		transform.transform *= glm::translate(velocity.velocity * _deltaTime); });
-}
-
-void System2::executeRotation(float _deltaTime) {
-	registry.execute<Transform, Rotation>([&](Transform& transform, Rotation& rotation) {
-		transform.transform = glm::rotate(transform.transform, rotation.angleInRadians * _deltaTime, rotation.axisOfRotation); });
-}
-
-void System2::removeEntityWhenNotInView(std::list<Entity>& entities){
-	int entityId = whichEntityIsNotInView();
-	if (entityId != -1) {
-		Entity entity{ static_cast<uint32_t>(entityId) };
-		eraseEntity(entity);
-		entities.remove_if([&entity](const Entity& _entity) { return _entity.id == entity.id; });
-	}
-}
-
-int System2::whichEntityIsNotInView() {
-	int found = -1;
-	registry.execute<Entity, Transform>([&found](Entity _entity, const Transform& transform) {
-			glm::vec3 position = glm::vec3(transform.transform[3][0], transform.transform[3][1], transform.transform[3][2]);
-			if (position[0] < (-75.f) || position[0] > (75.f) ||
-				position[1] < (-50.f) || position[1] > (50.f) ||
-				position[2] < (-100.f) || position[2] > (0.f))
-			{ 
-				found = _entity.id;
-				return found;
-			}});	
-	return found;
-}
-
-void System2::shootSphere(std::list<Entity>& _entities, float _velocity, const graphics::Texture2D& _texture) {
-	if (inputManager.isButtonPressed(input::MouseButton::LEFT)) {
-		glm::vec3 curserPos = camera.toWorldSpace(inputManager.getCursorPos());
-		Entity entity;
-		_entities.push_back(createEntity(entity));
-		addTexture(_entities.back(), &_texture);
-		addMesh(_entities.back(), "models/sphere.obj");
-		addTransform(_entities.back(), glm::translate(curserPos));		
-		addVelocity(_entities.back(), glm::vec3(curserPos[0] * _velocity, curserPos[1] * _velocity, curserPos[2] * _velocity));
-		addAABB(_entities.back(), true);
-	}
-}
-
-//void System2::rotate(Entity& _entity, float _deltatime) {
-//	Transform& transform = registry.getComponentUnsafe<Transform>(_entity);
-//	AngularVelocity& velo = registry.getComponentUnsafe<AngularVelocity>(_entity);
-//	glm::quat Quat = glm::quat(glm::vec3(velo.angular_velocity.x * _deltatime, velo.angular_velocity.y * _deltatime, velo.angular_velocity.z * _deltatime));
-//	glm::mat4 RotationMatrix = glm::toMat4(Quat);
-//	transform.transform *= RotationMatrix;
-//}
-
-void System2::updateAABB() {
-	registry.execute<Box, Transform>([&](Box& box, Transform transform) {
-		std::vector<glm::vec3> corners;
-		corners.push_back(glm::vec3(box.aabb.min.x, box.aabb.min.y, box.aabb.min.z));
-		corners.push_back(glm::vec3(box.aabb.min.x, box.aabb.min.y, box.aabb.max.z));
-		corners.push_back(glm::vec3(box.aabb.min.x, box.aabb.max.y, box.aabb.min.z));
-		corners.push_back(glm::vec3(box.aabb.min.x, box.aabb.max.y, box.aabb.max.z));
-		corners.push_back(glm::vec3(box.aabb.max.x, box.aabb.min.y, box.aabb.min.z));
-		corners.push_back(glm::vec3(box.aabb.max.x, box.aabb.min.y, box.aabb.max.z));
-		corners.push_back(glm::vec3(box.aabb.max.x, box.aabb.max.y, box.aabb.min.z));
-		corners.push_back(glm::vec3(box.aabb.max.x, box.aabb.max.y, box.aabb.max.z));
-		glm::vec3 current;
-		auto it = corners.begin();
-		glm::vec3 min(glm::vec3(camera.getViewProjection() * transform.transform * glm::vec4(*it, 1)));
-		glm::vec3 max(glm::vec3(camera.getViewProjection() * transform.transform * glm::vec4(*it, 1)));
-		it++;
-		for (; it != corners.end(); it++) {
-			current = glm::vec3(camera.getViewProjection() * transform.transform * glm::vec4(*it, 1));
-			if (current.x < min.x) min.x = current.x;
-			if (current.x > max.x) max.x = current.x;
-			if (current.y < min.y) min.y = current.y;
-			if (current.y > max.y) max.y = current.y;
-			if (current.z < min.z) min.z = current.z;
-			if (current.z > max.z) max.z = current.z;
-		}
-		box.transformedAabb.max = max;
-		box.transformedAabb.min = min;
-		});
-}
-
-
-int System2::removeIntersecting() {
-	utils::SparseOctree<Entity, 3, float> sparseOctree;
-	registry.execute<Entity, Box>([&](Entity ent, Box box) {
-		if (!box.isProjectile) sparseOctree.insert(box.transformedAabb, ent);
-		});
-	std::set<Entity> ent;
-	registry.execute<Box>([&](Box box) {
-		if (box.isProjectile) {
-			utils::SparseOctree<Entity, 3, float>::AABBQuery aabbQuery(box.transformedAabb);
-			sparseOctree.traverse(aabbQuery);
-			for (auto it = aabbQuery.hits.begin(); it != aabbQuery.hits.end(); it++) {
-				ent.insert(*it);
-			}
-		}
-		});
-	for (auto it = ent.begin(); it != ent.end(); it++) {
-		registry.erase(*it);
-	}
+	void shootSphere(std::list<Entity>& _entities, float _velocity, const graphics::Texture2D& _texture);
 	
-	return ent.size();
-}
+	//void rotate(Entity& _entity, float _deltatime);
 
-/* ################ Component-System ################ */
-void System2::addMesh(Entity& _entity, const char* _mesh) {
-	registry.addComponent<Mesh>(_entity, *utils::MeshLoader::get(_mesh));
-}
+	void updateAABB();
 
-void System2::addTransform(Entity& _entity, glm::mat4 _transfrom) {
-	registry.addComponent<Transform>(_entity, _transfrom);
-}
-
-void System2::setTransform(Entity& _entity, glm::mat4 _transform) {
-	Transform& transform = registry.getComponentUnsafe<Transform>(_entity);
-	transform.transform = _transform;
-}
-
-void System2::addTexture(Entity& _entity, const graphics::Texture2D* _texture) {
-	registry.addComponent<Texture>(_entity, _texture);
-}
-
-void System2::setTexture(Entity& _entity, const graphics::Texture2D* _texture) {
-	auto texture = registry.getComponentUnsafe<Texture>(_entity);
-	texture.texture = _texture;
-}
-
-void System2::addVelocity(Entity& _entity, glm::vec3 _velocity) {
-	registry.addComponent<Velocity>(_entity, _velocity);
-}
-
-void System2::setVelocity(Entity& _entity, glm::vec3 _velocity) {
-	Velocity& velocity = registry.getComponentUnsafe<Velocity>(_entity);
-	velocity.velocity = _velocity;
-}
-
-void System2::addAccelaration(Entity& _entity, glm::vec3 _velocity) {
-	registry.addComponent<Accelaration>(_entity, _velocity);
-}
-
-void System2::setAccelaration(Entity& _entity, glm::vec3 _accelaration) {
-	Accelaration& accelaration = registry.getComponentUnsafe<Accelaration>(_entity);
-	accelaration.accelaration = _accelaration;
-}
-
-void System2::addMass(Entity& _entity, float _mass) {
-	registry.addComponent<Mass>(_entity, _mass);
-}
-
-void System2::setMass(Entity& _entity, float _mass) {
-	Mass& mass = registry.getComponentUnsafe<Mass>(_entity);
-	mass.mass = _mass;
-}
-
-void System2::addAnchor(Entity& _entity, glm::vec3 _anchor) {
-	registry.addComponent<Anchor>(_entity, _anchor);
-}
-
-void System2::setAnchor(Entity& _entity, glm::vec3 _anchor) {
-	Anchor& anchor = registry.getComponentUnsafe<Anchor>(_entity);
-	anchor.anchor = _anchor;
-}
-
-void System2::addRotation(Entity& _entity, float _angleInRadians, glm::vec3 _axisOfRotation) {
-	registry.addComponent<Rotation>(_entity, _angleInRadians, _axisOfRotation);
-}
-
-void System2::setRotation(Entity& _entity, float _angleInRadians, glm::vec3 _axisOfRotation) {
-	Rotation& rotation = registry.getComponentUnsafe<Rotation>(_entity);
-	rotation.angleInRadians = _angleInRadians;
-	rotation.axisOfRotation = _axisOfRotation;
-}
-
-//void System2::addRotation(Entity& _entity, glm::vec3 _eulerAngles) {
-//	registry.addComponent<Rotation>(_entity, _eulerAngles);
-//}
-
-//void System2::addAngularVelocity(Entity& _entity, glm::vec3 _angular_velocity) {
-//	registry.addComponent<AngularVelocity>(_entity, _angular_velocity);
-//}
-
-void System2::addAABB(Entity& ent, bool isProjectile) {
-	graphics::Mesh& mesh = registry.getComponent<Mesh>(ent)->mesh;
-	glm::mat4 transform = camera.getViewProjection() * registry.getComponent<Transform>(ent)->transform;
-	auto it = mesh.vertices.begin();
-	glm::vec3 min(it->Position);
-	glm::vec3 max(it->Position);
-	glm::vec3 minTransformed(glm::vec3(transform * glm::vec4(it->Position, 1)));
-	glm::vec3 maxTransformed(glm::vec3(transform * glm::vec4(it->Position, 1)));
-	it++;
-	glm::vec3 current;
-	glm::vec3 currentTransformed;
-	for (; it != mesh.vertices.end(); it++) {
-		currentTransformed = glm::vec3(transform * glm::vec4(it->Position, 1));
-		if (currentTransformed.x < minTransformed.x) minTransformed.x = currentTransformed.x;
-		if (currentTransformed.x > maxTransformed.x) maxTransformed.x = currentTransformed.x;
-		if (currentTransformed.y < minTransformed.y) minTransformed.y = currentTransformed.y;
-		if (currentTransformed.y > maxTransformed.y) maxTransformed.y = currentTransformed.y;
-		if (currentTransformed.z < minTransformed.z) minTransformed.z = currentTransformed.z;
-		if (currentTransformed.z > maxTransformed.z) maxTransformed.z = currentTransformed.z;
-		current = it->Position;
-		if (current.x < min.x) min.x = current.x;
-		if (current.x > max.x) max.x = current.x;
-		if (current.y < min.y) min.y = current.y;
-		if (current.y > max.y) max.y = current.y;
-		if (current.z < min.z) min.z = current.z;
-		if (current.z > max.z) max.z = current.z;
-	}
-	registry.addComponent<Box>(ent, isProjectile, math::AABB<3>(min, max), math::AABB<3>(minTransformed, maxTransformed));
-}
+	int removeIntersecting();
 
 
-/* ################ Utils-System ################ */
-int System2::randomWithoutZero(int quantity, int start) {
-	int random = 0;
-	while (random == 0) {
-		random = rand() % quantity + (start);
-	};
-	return random;
-}
+	/* ################ Component-System ################ */
+	void addMesh(Entity& _entity, const char* _mesh);
+
+	void addTransform(Entity& _entity, glm::mat4 _transfrom);
+
+	void setTransform(Entity& _entity, glm::mat4 _transform);
+
+	void addTexture(Entity& _entity, const graphics::Texture2D* _texture);
+
+	void setTexture(Entity& _entity, const graphics::Texture2D* _texture);
+
+	void addVelocity(Entity& _entity, glm::vec3 _velocity);
+
+	void setVelocity(Entity& _entity, glm::vec3 _velocity);
+
+	void addAccelaration(Entity& _entity, glm::vec3 _velocity);
+
+	void setAccelaration(Entity& _entity, glm::vec3 _accelaration);
+
+	void addMass(Entity& _entity, float _mass);
+
+	void setMass(Entity& _entity, float _mass);
+
+	void addAnchor(Entity& _entity, glm::vec3 _anchor);
+
+	void setAnchor(Entity& _entity, glm::vec3 _anchor);
+
+	void addRotation(Entity& _entity, float _angleInRadians, glm::vec3 _axisOfRotation);
+
+	void setRotation(Entity& _entity, float _angleInRadians, glm::vec3 _axisOfRotation);
+
+	//void addRotation(Entity& _entity, glm::vec3 _eulerAngles);
+
+	//void addAngularVelocity(Entity& _entity, glm::vec3 _angular_velocity);
+
+	void addAABB(Entity& ent, bool isProjectile);
+
+
+	/* ################ Utils-System ################ */
+	int randomWithoutZero(int quantity, int start);
+
+	
+private:
+	Registry2 registry;
+	graphics::Camera camera;
+	graphics::MeshRenderer renderer;
+	input::InputManager inputManager;			
+
+	void executeVelocity(float _deltaTime);
+
+	void executeRotation(float _deltaTime);
+};
