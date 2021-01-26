d4dadd74a2b169e8127f0c78f8150174c93573e7
VolkerLue <65592732+VolkerLue@users.noreply.github.com>
2021-01-26 10:56:10 +0100

    Anpassung wegen Any

--- src/engine/game/Shooter.cpp
+++ src/engine/game/Shooter.cpp
@@ -3,16 +3,18 @@
 
 Shooter::Shooter() : GameState(), system(),
 texturePlanet(*graphics::Texture2DManager::get("textures/planet1.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
-textureCratetex(*graphics::Texture2DManager::get("textures/cratetex.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR)))
+textureCratetex(*graphics::Texture2DManager::get("textures/cratetex.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
+meshSphere(*utils::MeshLoader::get("models/sphere.obj")),
+meshCrate(*utils::MeshLoader::get("models/crate.obj")),
+shot(0)
 {
-	shot = 0;
 	srand(time(NULL));
 	for (int i = 0; i < 100; i++){
 		Entity entity;
 		entities.push_back(system.createEntity(entity));				
 
 		system.addTexture(entities.back(), &textureCratetex);
-		system.addMesh(entities.back(), "models/crate.obj");
+		system.addMesh(entities.back(), &meshCrate);
 		system.addTransform(entities.back(), glm::translate(glm::vec3(0.f, 0.f, float(rand() % 10 + (-55)))));
 		system.addVelocity(entities.back(), glm::vec3(system.randomWithoutZero(9, -4), system.randomWithoutZero(9, -4), system.randomWithoutZero(9, -4)));
 		system.addRotation(entities.back(), glm::radians(float(rand() % 20 + (10))), glm::vec3(rand() % 2, rand() % 2, rand() % 2));
@@ -30,7 +32,7 @@ void Shooter::update(float _time, float _deltaTime) {
 	system.removeEntityWhenNotInView(entities);
 
 	if (shot == 0 && input::InputManager::isButtonPressed(input::MouseButton::LEFT)) {
-		system.shootSphere(entities, 500.f, texturePlanet);
+		system.shootMeshWithTexure(&meshSphere, texturePlanet, entities, 500.f);
 		shot = 30;
 	}
 	else if (shot > 0) {
@@ -44,13 +46,12 @@ void Shooter::update(float _time, float _deltaTime) {
 		Entity entity;
 		entities.push_back(system.createEntity(entity));
 		system.addTexture(entities.back(), &textureCratetex);
-		system.addMesh(entities.back(), "models/crate.obj");
+		system.addMesh(entities.back(), &meshCrate);
 		system.addTransform(entities.back(), glm::translate(glm::vec3(0.f, 0.f, float(rand() % 10 + (-55)))));
 		system.addVelocity(entities.back(), glm::vec3(system.randomWithoutZero(9, -4), system.randomWithoutZero(9, -4), system.randomWithoutZero(9, -4)));
 		system.addRotation(entities.back(), glm::radians(float(rand() % 20 + (10))), glm::vec3(rand() % 2, rand() % 2, rand() % 2));
 		system.addAABB(entities.back(), false);
 	}
-	
 }
 
 void Shooter::draw(float _time, float _deltaTime) {
@@ -69,3 +70,4 @@ void Shooter::onPause() {
 bool Shooter::isFinished(){
 	return false;
 }
+
