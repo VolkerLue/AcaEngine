54f8c94b937324c12fe12947954f318b35855116
Janusz478 <64015397+Janusz478@users.noreply.github.com>
2021-01-26 22:42:52 +0100

    Update FreeFall.cpp

--- src/engine/game/FreeFall.cpp
+++ src/engine/game/FreeFall.cpp
@@ -3,16 +3,18 @@
 
 FreeFall::FreeFall() : GameState(), system(),
 texture(*graphics::Texture2DManager::get("textures/planet1.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
-texture2(*graphics::Texture2DManager::get("textures/cratetex.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR)))
+texture2(*graphics::Texture2DManager::get("textures/cratetex.png", graphics::Sampler(graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR, graphics::Sampler::Filter::LINEAR))),
+meshSphere(*utils::MeshLoader::get("models/sphere.obj")),
+meshCrate(*utils::MeshLoader::get("models/crate.obj"))
 {
 	entity = system.createEntity(entity);
-	system.addMesh(entity, "models/sphere.obj");
+	system.addMesh(entity, &meshSphere);
 	system.addTransform(entity, glm::translate(glm::vec3(0.f, 10.f, -50.f)));
 	system.addVelocity(entity, glm::vec3(0.f, 0.13f, 0.f));
 	system.addAccelaration(entity, glm::vec3(0.f, -0.1f, 0.f));
 	
 	entity2 = system.createEntity(entity2);
-	system.addMesh(entity2, "models/crate.obj");
+	system.addMesh(entity2, &meshCrate);
 	system.addTransform(entity2, glm::translate(glm::vec3(5.f, 10.f, -50.f)));
 	system.addVelocity(entity2, glm::vec3(0.f, 0.13f, 0.f));
 	system.addAccelaration(entity2, glm::vec3(0.f, -0.1f, 0.f));
